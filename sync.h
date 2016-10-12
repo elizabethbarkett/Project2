f1db4e40bea9b9763838bf0bb2bb3eab13a9f8fe
Elizabeth Barkett <e.barkett@vikes.csuohio.edu>
2016-10-11 23:55:06 -0400

    Update sync.h

--- sync.h
+++ sync.h
@@ -1,5 +1,5 @@
 /*
- * NAME, etc.
+ *
  *
  * sync.h
  */
@@ -8,8 +8,19 @@
 #define _STHREAD_SYNC_H_
 
 
+
 struct sthread_rwlock_struct {
-        /* FILL ME IN! */
+    /* FILL ME IN! 
+	read_lock is 0 or 1
+	write_lock is 0 or 1
+	make wait queue for threads
+	call test and set with lock
+    */
+    unsigned long  *lock;
+    sthread_t *wait_queue;
+    int *front_of_queue;//next thread in queue to be woken
+    int *count_variable;//back of queue, next open spot to add to queue
+    sthread_t thread_id;
 };
 
 typedef struct sthread_rwlock_struct sthread_rwlock_t;
@@ -24,3 +35,4 @@ int sthread_write_try_lock(sthread_rwlock_t *rwlock);
 int sthread_write_unlock(sthread_rwlock_t *rwlock);
 
 #endif
+
